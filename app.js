f40b4375242711896e8b89178c60b29cf16d4323
Jhon Duarte <jhonny.jdt18@gmail.com>
2024-03-26 09:42:36 +0100

    conociendo los diferente tipos de selectores

---
+++ app.js
@@ -0,0 +1,8 @@
+const s = 'Holaaaaa';
+console.log(s);
+
+
+const a = 2;
+const b = 2;
+const suma = a+b;
+console.log(suma);
---
+++ src/assets/main.js
@@ -0,0 +1,6 @@
+const primerTopping = document.querySelector('.tooping');
+
+primerTopping.style.color = 'blue';
+
+const saludo = 'Hola Mundo';
+console.log(saludo);
